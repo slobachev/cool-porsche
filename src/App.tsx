a1ced5a7a825d3f9f8f549ef6627c8e816730398
slobachev <sergeylobachev@outlook.com>
2024-05-10 20:10:01 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -13,10 +13,8 @@ import { useEffect, useRef, useState } from 'react';
 import { HexColorPicker } from 'react-colorful';
 import { proxy, useSnapshot } from 'valtio';
 
-// Using a Valtio state model to bridge reactivity between
-// the canvas and the dom, both can write to it and/or react to it.
 const state = proxy({
-  current: null,
+  current: '',
   items: {
     trim: '#ffffff',
     wheels: '#ffffff',
@@ -35,9 +33,6 @@ const state = proxy({
 function Car() {
   const ref = useRef();
   const snap = useSnapshot(state);
-  // Drei's useGLTF hook sets up draco automatically, that's how it differs from useLoader(GLTFLoader, url)
-  // { nodes, materials } are extras that come from useLoader, these do not exist in threejs/GLTFLoader
-  // nodes is a named collection of meshes, materials a named collection of materials
   const { nodes, materials } = useGLTF('porsche.glb');
   const {
     trim,
@@ -53,27 +48,23 @@ function Car() {
     headlightsGlass,
     rims: wheels,
   } = nodes || {};
+  const [hovered, setHovered] = useState(null);
 
-  // Animate model
-
-  // Cursor showing current color
-  const [hovered, set] = useState(null);
   useEffect(() => {
     const cursor = `<svg width="64" height="64" fill="none" xmlns="http://www.w3.org/2000/svg"><g clip-path="url(#clip0)"><path fill="rgba(255, 255, 255, 0.5)" d="M29.5 54C43.031 54 54 43.031 54 29.5S43.031 5 29.5 5 5 15.969 5 29.5 15.969 54 29.5 54z" stroke="#000"/><g filter="url(#filter0_d)"><path d="M29.5 47C39.165 47 47 39.165 47 29.5S39.165 12 29.5 12 12 19.835 12 29.5 19.835 47 29.5 47z" fill="${snap.items[hovered]}"/></g><path d="M2 2l11 2.947L4.947 13 2 2z" fill="#000"/><text fill="#000" style="white-space:pre" font-family="Inter var, sans-serif" font-size="10" letter-spacing="-.01em"><tspan x="35" y="63">${hovered}</tspan></text></g><defs><clipPath id="clip0"><path fill="#fff" d="M0 0h64v64H0z"/></clipPath><filter id="filter0_d" x="6" y="8" width="47" height="47" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB"><feFlood flood-opacity="0" result="BackgroundImageFix"/><feColorMatrix in="SourceAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/><feOffset dy="2"/><feGaussianBlur stdDeviation="3"/><feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.15 0"/><feBlend in2="BackgroundImageFix" result="effect1_dropShadow"/><feBlend in="SourceGraphic" in2="effect1_dropShadow" result="shape"/></filter></defs></svg>`;
     const auto = `<svg width="64" height="64" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill="rgba(255, 255, 255, 0.5)" d="M29.5 54C43.031 54 54 43.031 54 29.5S43.031 5 29.5 5 5 15.969 5 29.5 15.969 54 29.5 54z" stroke="#000"/><path d="M2 2l11 2.947L4.947 13 2 2z" fill="#000"/></svg>`;
     document.body.style.cursor = `url('data:image/svg+xml;base64,${btoa(
       hovered ? cursor : auto,
     )}'), auto`;
-  }, [hovered]);
+  }, [hovered, snap.items]);
 
-  // Using the GLTFJSX output here to wire in app-state and hook up events
   return (
     <group
       ref={ref}
       dispose={null}
-      onPointerOver={(e) => (e.stopPropagation(), set(e.object.material.name))}
-      onPointerOut={(e) => e.intersections.length === 0 && set(null)}
-      onPointerMissed={() => (state.current = null)}
+      onPointerOver={(e) => (e.stopPropagation(), setHovered(e.object.material.name))}
+      onPointerOut={(e) => e.intersections.length === 0 && setHovered(null)}
+      onPointerMissed={() => (state.current = '')}
       onPointerDown={(e) => (e.stopPropagation(), (state.current = e.object.material.name))}
     >
       {trim?.children?.map((m) => (
